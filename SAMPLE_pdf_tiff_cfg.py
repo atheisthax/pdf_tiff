64c4b9b3a6a5b938a7d7d2f12678b0035f7a6fe7
atheisthax <maxim.vasilich@gmail.com>
2021-12-10 12:05:29 +0300

    added fixed sample config file

---
+++ SAMPLE_pdf_tiff_cfg.py
@@ -0,0 +1,7 @@
+mailserver = "MAIL.SERVER.ADDRESS"
+emailaddress = "pdftiff@yourdomain"
+maillogin = "pdftiff"
+mailpassword = "yourpassword"
+imgtype = "bilevel"
+imgcompression = "fax"
+resolution = 300
